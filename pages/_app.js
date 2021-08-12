6c4d0ffb94f7798da49a956e178133dc51ee8c30
Tom <tom.jackson.tc@btinternet.com>
2021-08-12 17:14:58 +0100

    fixed for vercel deployment

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,12 @@ import { SWRConfig } from "swr";
 import axios from "axios";
 import "../styles/globals.css";
 
-axios.defaults.baseURL = `${process.env.BASE_URL}/api`;
+axios.defaults.baseURL =
+  process.env.VERCEL_ENV === "production"
+    ? process.env.VERCEL_URL + "/api"
+    : process.env.BASE_URL
+    ? process.env.BASE_URL + "/api"
+    : "http://localhost:3000/api";
 
 function MyApp({ Component, pageProps }) {
   return (
