f2d9115f0249ef88c89f7273eba06d2d5e898d80
Jom185 <76267367+Jom185@users.noreply.github.com>
2021-09-10 09:44:30 +0100

    before test, update

--- pages/api/folder/update.js
+++ pages/api/folder/update.js
@@ -2,7 +2,6 @@ import nextConnect from "next-connect";
 import passport from "../../../lib/passport";
 
 import Flashcard from "../../../models/Flashcard";
-import User from "../../../models/User";
 
 const handler = nextConnect()
   .use(passport.initialize())
@@ -17,16 +16,37 @@ const handler = nextConnect()
             err: err.message || "Server error",
           });
         }
-        if (!user) {
-          return res.status(401).json({ message: "Unauthorised" });
-        }
+        if (!user) return res.status(401).json({ message: "Unauthorised" });
         // req: {
         // folder
         // flashcards: [{front, back, id}]
-
         // }
-        const newCards = req.body.flashcards.filter((card) => !!card._id);
-        for (const card in newCards) {
+        for (const card in req.body.flashcards) {
+          if (card?._id) {
+            Flashcard.findOne({ _id: card._id }).exec(
+              async (err, existingCard) => {
+                if (err) {
+                  return res.status(500).json({
+                    message: error || "Something happend",
+                    error: err.message || "Server error",
+                  });
+                }
+                existingCard = {
+                  ...existingCard,
+                  front: card.front,
+                  back: card.back,
+                };
+                existingCard.save((error) => {
+                  if (err) {
+                    return res.status(500).json({
+                      message: error || "Something happend",
+                      error: err.message || "Server error",
+                    });
+                  }
+                });
+              }
+            );
+          }
           const newCard = new Flashcard({
             ...card,
             folder: req.body.folder,
@@ -42,34 +62,8 @@ const handler = nextConnect()
             }
           });
         }
-        const existingCards = req.body.flashcards.filter((card) => !card._id);
-        for (const card in existingCards) {
-          // get card, update it and save it
-          Flashcard.findOne({ _id: card._id }).exec(
-            async (err, existingCard) => {
-              if (err) {
-                return res.status(500).json({
-                  message: error || "Something happend",
-                  error: err.message || "Server error",
-                });
-              }
-              existingCard = {
-                ...existingCard,
-                front: card.front,
-                back: card.back,
-              };
-              existingCard.save((error) => {
-                if (err) {
-                  return res.status(500).json({
-                    message: error || "Something happend",
-                    error: err.message || "Server error",
-                  });
-                }
-              });
-            }
-          );
-        }
-        return res.json({success: true})
+        return res.json({ success: true });
+      }
     )(req, res);
   });
 
