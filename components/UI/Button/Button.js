c5f07cba18d1988cc00a15de0939e3844a9219cc
Tom <tom.jackson.tc@btinternet.com>
2021-12-12 17:30:22 +0000

    fixed styling

--- components/UI/Button/Button.js
+++ components/UI/Button/Button.js
@@ -11,13 +11,13 @@ export default function Button ({
     <button
       disabled={disabled}
       type="button"
-      className={`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white ${
+      className={`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium ${
         disabled
           ? 'border-gray-300 bg-gray-400 text-gray-300 cursor-not-allowed'
           : danger
-            ? 'hover:bg-red-700 focus:ring-red-500 bg-red-600'
+            ? 'hover:bg-red-700 focus:ring-red-500 bg-red-600 text-white'
             : main
-              ? 'hover:bg-sky-700 focus:ring-sky-500 bg-sky-600'
+              ? 'hover:bg-sky-700 focus:ring-sky-500 bg-sky-600 text-white'
               : ' border-gray-300 shadow-sm bg-white text-base text-gray-700 hover:bg-gray-50  focus:ring-sky-500 '
       } focus:outline-none focus:ring-2 focus:ring-offset-2  sm:ml-3 sm:w-auto sm:text-sm
       `}
