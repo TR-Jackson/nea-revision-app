93053606e2c6c6016fa44d521931f0bec5361bc6
Tom <tom.jackson.tc@btinternet.com>
2022-02-27 17:52:16 +0000

    added progress chart

--- components/Folder/Folder.js
+++ components/Folder/Folder.js
@@ -1,8 +1,14 @@
+import 'chart.js/auto'
 import PropTypes from 'prop-types'
-
+import { useState } from 'react'
 import { FolderOpenIcon, BookOpenIcon } from '@heroicons/react/outline'
+import { Bar } from 'react-chartjs-2'
+
+import ExpandChevron from '../UI/ExpandChevron/ExpandChevron'
 
 export default function Folder ({ name, description, revisedStatus, openFolderHandler, toRevise }) {
+  const [isExpanded, setIsExpanded] = useState(false)
+
   return (
     <div className="relative flex justify-center">
       <div
@@ -26,9 +32,49 @@ export default function Folder ({ name, description, revisedStatus, openFolderHa
           <p className="text-lg truncate grow">{description}</p>
         </div>
         <div className="flex-grow flex my-auto justify-end space-x-4 pr-6 shrink-0">
-          {revisedStatus.map((count, i) => (
-            <p key={i}>{count}</p>
-          ))}
+          <div className=' w-auto'>
+            {isExpanded && (
+              <>
+                <div className='font-semibold'>Number of correct days of repetion</div>
+                <Bar
+                  data={{
+                    labels: ['Unrevised', '0', '1', '2 to 3', '4 to 8', '8+'],
+                    datasets: [
+                      {
+                        label: 'Number of flashcards',
+                        data: revisedStatus,
+                        backgroundColor: [
+                          '#ededed',
+                          '#c40000',
+                          '#c45200',
+                          '#edd100',
+                          '#7fba00',
+                          '#00ba06'
+                        ],
+                        hoverOffset: 10,
+                        borderWidth: 0
+                      }
+                    ]
+                  }} options={{
+                    plugins: {
+                      legend: {
+                        display: false
+                      }
+                    },
+                    scales: {
+                      yAxes: {
+                        display: false
+                      }
+                    }
+                  }}/>
+              </>
+            )}
+          </div>
+          <div className='h-6 w-6'>
+            <ExpandChevron
+              expanded={isExpanded} onToggle={() => setIsExpanded(!isExpanded)}
+            />
+          </div>
         </div>
       </div>
       {toRevise && <div className="w-auto justify-end flex">
