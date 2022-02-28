cb7c775a50d84a2ce45cde57a4e3d86d160bb9ea
Tom <tom.jackson.tc@btinternet.com>
2022-02-28 19:43:33 +0000

    improved styling

--- components/Folder/Folder.js
+++ components/Folder/Folder.js
@@ -32,52 +32,52 @@ export default function Folder ({ name, description, revisedStatus, openFolderHa
             <p className='font-semibold truncate text-xl grow-0'>{name}</p>
             <p className='text-lg truncate grow'>{description}</p>
           </div>
-          <div className='flex-grow top-0 right-0'>
+          <div className='flex-grow flex flex-row-reverse my-auto space-x-4 pr-6 shrink-0'>
+            <div className='w-auto'>
+              {isExpanded && (
+                <>
+                  <div className='font-semibold'>Number of correct days of repetition</div>
+                  <Bar
+                    data={{
+                      labels: ['Unrevised', '0', '1', '2 to 3', '4 to 8', '8+'],
+                      datasets: [
+                        {
+                          label: 'Number of flashcards',
+                          data: revisedStatus,
+                          backgroundColor: [
+                            '#ededed',
+                            '#c40000',
+                            '#c45200',
+                            '#edd100',
+                            '#7fba00',
+                            '#00ba06'
+                          ],
+                          hoverOffset: 10,
+                          borderWidth: 0
+                        }
+                      ]
+                    }} options={{
+                      plugins: {
+                        legend: {
+                          display: false
+                        }
+                      },
+                      scales: {
+                        yAxes: {
+                          display: false
+                        }
+                      }
+                    }}/>
+                </>
+              )}
+            </div>
+          </div>
+          <div className=' flex flex-row-reverse'>
             <div className='h-6 w-6'>
               <ExpandChevron
                 expanded={isExpanded} onToggle={() => setIsExpanded(!isExpanded)}
               />
             </div>
-          </div>
-        </div>
-        <div className='flex-grow flex my-auto justify-end space-x-4 pr-6 shrink-0'>
-          <div className='w-auto place-self-center'>
-            {isExpanded && (
-              <>
-                <div className='font-semibold'>Number of correct days of repetition</div>
-                <Bar
-                  data={{
-                    labels: ['Unrevised', '0', '1', '2 to 3', '4 to 8', '8+'],
-                    datasets: [
-                      {
-                        label: 'Number of flashcards',
-                        data: revisedStatus,
-                        backgroundColor: [
-                          '#ededed',
-                          '#c40000',
-                          '#c45200',
-                          '#edd100',
-                          '#7fba00',
-                          '#00ba06'
-                        ],
-                        hoverOffset: 10,
-                        borderWidth: 0
-                      }
-                    ]
-                  }} options={{
-                    plugins: {
-                      legend: {
-                        display: false
-                      }
-                    },
-                    scales: {
-                      yAxes: {
-                        display: false
-                      }
-                    }
-                  }}/>
-              </>
-            )}
           </div>
         </div>
       </div>
