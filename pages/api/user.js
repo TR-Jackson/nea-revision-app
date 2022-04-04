9480b67b0068885b3e634fd3bdcc2d1fb98d8e18
Tom <tom.jackson.tc@btinternet.com>
2022-04-04 18:05:56 +0100

    Added comments

--- pages/api/user.js
+++ pages/api/user.js
@@ -3,6 +3,8 @@ import passport from '../../lib/passport'
 
 import { checkAuthError, checkAuthorised } from '../../util/errors'
 
+// API route for checking a user's JWT cookie (i.e. that they are logged on)
+// Used by all pages that display user specific content
 const handler = nextConnect()
   .use(passport.initialize())
   .get((req, res) => {
@@ -13,11 +15,11 @@ const handler = nextConnect()
         try {
           checkAuthError(err)
           checkAuthorised(user)
-
+          // Returns the user's username as an indicator that they are authenticated
           return res.json({ username: user.username })
         } catch (error) {
-          if (error.status === 401) res.json(false)
-          else return res.status(error.status).json({ message: error.message })
+          if (error.status === 401) res.json(false) // If JWT is invalid then return false for use in the useUser hook
+          else return res.status(error.status).json({ message: error.message }) // Return error if actual error has occurred
         }
       }
     )(req, res)
